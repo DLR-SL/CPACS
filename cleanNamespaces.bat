1bf2d5a1c1b5706508e4aeb2334a9bf7913acaef
Axel Berres <Axel.Berres@dlr.de>
2018-05-08 09:53:52 +0200

    Change tools dependency

--- cleanNamespaces.bat
+++ cleanNamespaces.bat
@@ -1,2 +1,2 @@
 python .\converter\convert.py
-java -jar D:\Projekte\CPACS\Tools\SaxonHE9-6-0-8J\saxon9he.jar -s:.\schema\cpacs_schema.xsd -xsl:.\converter\namespace-cleanup.xsl -o:.\schema\cpacs_schema.xsd
+java -jar .\tools\SaxonHE9-6-0-8J\saxon9he.jar -s:.\schema\cpacs_schema.xsd -xsl:.\converter\namespace-cleanup.xsl -o:.\schema\cpacs_schema.xsd
