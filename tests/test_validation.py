764e3394d7c9cc7ea77275949d5735ddd09ec448
Alder, Marko <marko.alder@dlr.de>
2019-12-17 13:18:30 +0100

    add ribs/spars examples

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -11,9 +11,27 @@ class schemaCheck(unittest.TestCase):
             result = True
         self.assertTrue(result)
         tixi_h.close()
-        
+
     def test_basicWing(self):
         xml = '../examples/basicWing.xml'
+        xsd = '../schema/cpacs_schema.xsd'
+        tixi_h.open(xml)
+        if not tixi_h.schemaValidateFromFile(xsd):
+            result = True
+        self.assertTrue(result)
+        tixi_h.close()
+
+    def test_wing_cell_rib_spar(self):
+        xml = '../examples/wing_cell_rib_spar.xml'
+        xsd = '../schema/cpacs_schema.xsd'
+        tixi_h.open(xml)
+        if not tixi_h.schemaValidateFromFile(xsd):
+            result = True
+        self.assertTrue(result)
+        tixi_h.close()
+
+    def test_wing_explicitRibPositioning(self):
+        xml = '../examples/wing_explicitRibPositioning.xml'
         xsd = '../schema/cpacs_schema.xsd'
         tixi_h.open(xml)
         if not tixi_h.schemaValidateFromFile(xsd):
