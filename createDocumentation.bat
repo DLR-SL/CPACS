cd7c6cd2d7fc47b242149cd1d17950cdca4b8f6d
Alder <Marko.Alder@dlr.de>
2023-10-28 20:14:59 +0200

    documentation//Cpacs_doc_project.shfbproj

--- createDocumentation.bat
+++ createDocumentation.bat
@@ -1,4 +1,9 @@
-REM for first time user, first unzip the file *development/3rdparty.zip*!
-REM potentially remove path to installed SandCastle from environment variable as the build might not success.
+@echo off
+REM Make sure that 7z and msbuild are installed and available as commands (e.g., by setting the paths to the corresponding tools in the PATH variable).
+REM Alternatively, replace these with the full paths to 7z.exe and MSBuild.exe.  
 
-MSBuild.exe .\documentation\Cpacs_doc_project.shfbproj
+REM Extract 3rdparty.zip:
+7z x -y .\development\3rdparty.zip -odevelopment\
+
+REM Build documentation
+msbuild /p:Configuration=Release documentation\Cpacs_doc_project.shfbproj
