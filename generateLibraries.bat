4fee64330f9f059faf79595c902d06a2d3142652
MarAlder <43143741+MarAlder@users.noreply.github.com>
2026-08-02 22:50:23 +0200

    rm generate libraries batch script

--- generateLibraries.bat
+++
@@ -1,4 +0,0 @@
-md build\lib\Python
-md build\lib\Java
-python D:\libs\generateDS-2.7b\generateDS.py -f -o .\build\lib\Python\cpacslib.py .\schema\cpacs_schema.xsd
-D:\libs\jaxb-ri\bin\xjc.bat -d .\build\lib -p Java .\schema\cpacs_schema.xsd
